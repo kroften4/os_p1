986e513437185ab8086b2f449a323c676352264c
Stanislav <sskozhemyakin2006@gmail.com>
2026-03-19 13:22:14 +0300

    add workers

--- src/main.c
+++ src/main.c
@@ -2,24 +2,17 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include "progress.h"
 #include "fsize.h"
 #include "xor/xor.h"
-#include "queue/ts_queue.h"
-#include "buf.h"
 
+#define NUM_WORKERS 3
 #define EXIT_SIG(sig) (128 + sig)
-
 #define IO_BUF_SIZE 4096
-#define PROGRESS_BAR_LEN 10
-#define PROGRESS_INTERVAL_PERCENT 5
 
-static pthread_cond_t not_empty = PTHREAD_COND_INITIALIZER;
-
-static bool is_eof = false;
 static volatile bool is_interrupted = false;
 
 void sigint_handler(int)
@@ -27,18 +20,18 @@ void sigint_handler(int)
 	is_interrupted = true;
 }
 
-struct encrypt_file_args {
-	FILE *fp;
-	struct ts_queue *q;
+pthread_mutex_t logfile_mtx = PTHREAD_MUTEX_INITIALIZER;
+
+pthread_mutex_t curr_file_mtx = PTHREAD_MUTEX_INITIALIZER;
+struct process_file_args {
+	FILE *logfile;
+	char **filenames;
+	char *out_dir;
+	size_t file_amount;
+	size_t *curr_file_idx;
 	uint8_t key;
 };
-void *encrypt_file(struct encrypt_file_args *args);
-
-struct write_file_args {
-	FILE *fp;
-	struct ts_queue *q;
-};
-void *write_file(struct write_file_args *args);
+void *process_file(struct process_file_args *args);
 
 int main(int argc, char *argv[])
 {
@@ -46,153 +39,123 @@ int main(int argc, char *argv[])
 		perror("sigint handler");
 		return EXIT_FAILURE;
 	}
-	if (argc != 4) {
-		printf("Usage: %s <input file> <output file> <key>\n", argv[0]);
+	if (argc < 4) {
+		printf("Usage: %s <file_1> [... <file_n>] <out_dir> <key>\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 
-	char *src_file = argv[1];
-	char *dst_file = argv[2];
-	uint8_t key = argv[3][0];
+	const size_t file_amount = argc - 3; // comand, out_dir, key
+	size_t next_file_idx = 0;
 
-	FILE *src_fp = fopen(src_file, "rb");
-	if (src_fp == NULL) {
-		perror(src_file);
-		return EXIT_FAILURE;
+	char *out_dir = argv[argc - 2];
+	uint8_t key = argv[argc - 1][0];
+	xor_set_key(key);
+
+	FILE *log_fp = fopen("log.txt", "w");
+
+	char **filenames = argv + 1;
+	for (size_t i = 0; i < file_amount; i++) {
+		printf("%zu: %s\n", i, filenames[i]);
 	}
 
-	FILE *dst_fp = fopen(dst_file, "wb");
-	if (src_fp == NULL) {
-		perror(dst_file);
-		return EXIT_FAILURE;
+	struct process_file_args args = { .file_amount = file_amount,
+									  .filenames = filenames,
+									  .out_dir = out_dir,
+									  .curr_file_idx = &next_file_idx,
+									  .logfile = log_fp,
+									  .key = key };
+
+	pthread_t workers[NUM_WORKERS];
+	for (size_t i = 0; i < NUM_WORKERS; i++) {
+		pthread_create(&workers[i], NULL, (void *(*)(void *))process_file,
+					   &args);
 	}
 
-	struct ts_queue *queue = ts_queue_new();
-	queue->data_destructor = (void (*)(void *))buf_destruct;
-
-	struct encrypt_file_args producer_args = {
-		.fp = src_fp,
-		.q = queue,
-		.key = key,
-	};
-	pthread_t producer;
-	pthread_create(&producer, NULL, (void *(*)(void *))encrypt_file,
-				   &producer_args);
-
-	struct write_file_args consumer_args = {
-		.fp = dst_fp,
-		.q = queue,
-	};
-	pthread_t consumer;
-	pthread_create(&consumer, NULL, (void *(*)(void *))write_file,
-				   &consumer_args);
-
-	pthread_join(producer, NULL);
-	pthread_join(consumer, NULL);
-
+	for (size_t i = 0; i < NUM_WORKERS; i++) {
+		pthread_join(workers[i], NULL);
+	}
 	if (is_interrupted) {
-		printf("Gracefully stopping...\n");
-	}
-
-	ts_queue_destroy(queue);
-
-	printf("Closing file descriptors\n");
-	bool failure = false;
-	if (fclose(src_fp) != 0) {
-		perror(src_file);
-		failure = true;
-	}
-	if (fclose(dst_fp) != 0) {
-		perror(dst_file);
-		failure = true;
-	}
-	if (failure) {
-		return EXIT_FAILURE;
-	}
-
-	if (is_interrupted) {
+		printf("Interrupted\n");
 		return EXIT_SIG(SIGINT);
 	}
 
 	return EXIT_SUCCESS;
 }
 
-void *write_file(struct write_file_args *args)
+void write_log(FILE *logfile, char *filename, char *msg, pthread_mutex_t *mtx)
 {
-	struct buf *buf = NULL;
-	struct ts_queue *q = args->q;
-	while (1) {
-		pthread_mutex_lock(&q->mutex);
-		{
-			while (__ts_queue_is_empty(q) && !is_eof && !is_interrupted) {
-				pthread_cond_wait(&not_empty, &q->mutex);
-			}
-			if ((__ts_queue_is_empty(q) && is_eof) || is_interrupted) {
-				pthread_mutex_unlock(&q->mutex);
-				break;
-			}
-			buf = q->head->data;
-		}
-		pthread_mutex_unlock(&q->mutex);
-
-		(void)fwrite(buf->data, sizeof(buf->data[0]), buf->size, args->fp);
-		ts_queue_dequeue(q);
-		// sleep(1);
+	if (pthread_mutex_trylock(mtx) != 0) {
+		(void)fprintf(stderr, "trylock writelog\n");
 	}
-	return NULL;
+	char timebuf[64];
+	time_t now = time(NULL);
+	(void)strftime(timebuf, sizeof(timebuf), "%Y-%m-%d %H:%M:%S",
+				   localtime(&now));
+	(void)fprintf(logfile, "TIME: %s TID: %lu FILE: %s MSG: %s\n", timebuf,
+				  pthread_self(), filename, msg);
+	pthread_mutex_unlock(mtx);
 }
 
-void *encrypt_file(struct encrypt_file_args *args)
+void *process_file(struct process_file_args *args)
 {
-	struct buf *buf = NULL;
-	struct ts_queue *q = args->q;
-	xor_set_key(args->key);
-	int size = fsize(args->fp);
-	int written = 0;
-	int last_percent = -PROGRESS_INTERVAL_PERCENT;
-	int curr_percent = 0;
+	char *filename = "";
+	uint8_t *buf = NULL;
 	while (1) {
 		if (is_interrupted) {
 			break;
 		}
-
-		buf = buf_new(IO_BUF_SIZE);
-
-		size_t len =
-			fread(buf->data, sizeof(buf->data[0]), buf->size, args->fp);
-		// sleep(1);
-		if (len == 0) {
-			pthread_mutex_lock(&q->mutex);
-			{
-				is_eof = true;
-				pthread_cond_signal(&not_empty);
-			}
-			pthread_mutex_unlock(&q->mutex);
-			buf_destruct(buf);
-			break;
+		if (pthread_mutex_trylock(&curr_file_mtx) != 0) {
+			(void)fprintf(stderr, "trylock\n");
 		}
-		buf->size = len;
-		xor_encrypt(buf->data, buf->data, buf->size);
-		written += len;
-
-		curr_percent = (float)written / size * 100;
-		if (curr_percent - last_percent >= PROGRESS_INTERVAL_PERCENT) {
-			printf("\r[");
-			progress_print_bar(written, size, PROGRESS_BAR_LEN, NULL);
-			printf("]");
-			printf(" %d%%", curr_percent);
-			(void)fflush(stdout);
-			last_percent = curr_percent;
-		}
-
-		pthread_mutex_lock(&q->mutex);
+		// write_log(args->logfile, filename, "starting write", &logfile_mtx);
+		(void)fprintf(stderr, "written log\n");
 		{
-			__ts_queue_enqueue_nolock(q, buf);
-			pthread_cond_signal(&not_empty);
+			if (*args->curr_file_idx >= args->file_amount) {
+				pthread_mutex_unlock(&curr_file_mtx);
+				break;
+			}
+			filename = args->filenames[*args->curr_file_idx];
+			(void)fprintf(stderr, "filename: %s\n", filename);
+			*args->curr_file_idx += 1;
 		}
-		pthread_mutex_unlock(&q->mutex);
-	}
-	printf("\n");
+		pthread_mutex_unlock(&curr_file_mtx);
+		(void)fprintf(stderr, "unlocked\n");
+		FILE *src_fp = fopen(filename, "rb");
+		if (src_fp == NULL) {
+			perror(filename);
+			write_log(args->logfile, filename, "error opening file",
+					  &logfile_mtx);
+			continue;
+		}
 
+		// TODO: add max buf size
+		size_t buflen = fsize(src_fp);
+		(void)fprintf(stderr, "buflen: %zu\n", buflen);
+		buf = malloc(buflen);
+
+		(void)fread(buf, sizeof(uint8_t), buflen, src_fp);
+		(void)fclose(src_fp);
+
+		xor_encrypt(buf, buf, buflen);
+
+		size_t full_path_len = strlen(args->out_dir) + strlen(filename) + 2;
+		char *full_path = malloc(full_path_len);
+		(void)snprintf(full_path, full_path_len, "%s/%s", args->out_dir,
+					   filename);
+		FILE *dst_fp = fopen(full_path, "wb");
+		if (dst_fp == NULL) {
+			perror(full_path);
+			write_log(args->logfile, filename, "error writing file",
+					  &logfile_mtx);
+			goto cleanup;
+		}
+		(void)fwrite(buf, sizeof(uint8_t), buflen, dst_fp);
+
+		(void)fclose(dst_fp);
+
+		// write_log(args->logfile, filename, "done writing", &logfile_mtx);
+cleanup:
+		free(buf);
+	}
 	return NULL;
 }
